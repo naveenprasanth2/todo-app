39a4c201e202c50df1e38930dbe0f49cc1edb249
naveenprasanth2 <78147993+naveenprasanth2@users.noreply.github.com>
2023-04-01 14:15:09 +0530

    tillHeader

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,21 +1,22 @@
 import { useState } from "react"
 import "./TodoApp.css"
-import { BrowserRouter, Route, Routes, useNavigate, useParams } from "react-router-dom"
+import { BrowserRouter, Link, Route, Routes, useNavigate, useParams } from "react-router-dom"
 
 export default function TodoApp() {
     return (
         <>
             <BrowserRouter>
+            <HeaderComponent />
                 <Routes>
                     <Route path="/" element={<LoginComponent />} />
                     <Route path="/login" element={<LoginComponent />} />
                     <Route path="/welcome/:userName" element={<WelcomeComponent />} />
                     <Route path="/todos" element={<ListTodosComponent />} />
+                    <Route path="/logout" element={<LogoutComponent />} />
                     <Route path="*" element={<ErrorComponent />} />
                 </Routes>
+                <FooterComponent />
             </BrowserRouter>
-
-            {/* <WelcomeComponent/> */}
         </>
     )
 }
@@ -73,7 +74,7 @@ function WelcomeComponent() {
     return (
         <>
             <h1>Welcome {userName}</h1>
-            <div className="Welcome">Welcome Component</div>
+            <div className="Welcome">Manage your todos - <Link to="/todos">Go Here</Link></div>
         </>
     )
 }
@@ -90,32 +91,90 @@ function ErrorComponent() {
 
 function ListTodosComponent() {
 
-    const todos = [{ id: 1, description: 'Learn AWS' },
-                    { id: 2, description: 'Learn Full Stack dev' },
-                    { id: 3, description: 'Learn DevOps' }
-                    ]
+    const today = new Date()
+    const targetDate = new Date(today.getFullYear() + 12, today.getMonth(), today.getDay())
+
+
+    const todos = [{ id: 1, description: 'Learn AWS', done: false, targetDate: targetDate },
+    { id: 2, description: 'Learn Full Stack dev', done: false, targetDate: targetDate },
+    { id: 3, description: 'Learn DevOps', done: false, targetDate: targetDate }
+    ]
 
     return (
-        <div className="todosComponent">
+        <div className="container">
             <h1>Things you want to do</h1>
             <div>
 
-                <table>
+                <table className="table">
                     <thead>
                         <tr>
-                            <td>id</td>
-                            <td>description</td>
+                            <td>ID</td>
+                            <td>Description</td>
+                            <td>Is Done?</td>
+                            <td>Target Date</td>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>{todos[0].id}</td>
-                            <td>{todos[0].description}</td>
-                        </tr>
+                        {
+                            todos.map((todo) => (
+                                <tr key={todo.id}>
+                                    <td>{todo.id}</td>
+                                    <td>{todo.description}</td>
+                                    <td>{todo.done.toString()}</td>
+                                    <td>{todo.targetDate.toDateString()}</td>
+                                </tr>
+                            )
+                            )
+                        }
+
                     </tbody>
                 </table>
 
             </div>
+        </div>
+    )
+}
+
+function HeaderComponent() {
+    return (
+        <header className="border-bottom border-light border-5 mb-5 p-2">
+        <div className="container">
+            <div className="row">
+                <nav className="navbar navbar-expand-lg">
+                    <a className="navbar-brand ms-2 fs-2 fw-bold text-black" href="https://www.in28minutes.com">in28minutes</a>
+                    <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav">
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/welcome/in28minutes">Home</Link></li>
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/todos">Todos</Link></li>
+                        </ul>
+                    </div>
+                    <ul className="navbar-nav">
+                        <li className="nav-item fs-5"><Link className="nav-link" to="/login">Login</Link></li>
+                        <li className="nav-item fs-5"><Link className="nav-link" to="/logout">Logout</Link></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    )
+}
+
+function FooterComponent() {
+    return (
+        <footer className="footer">
+            <div className="container">
+                Your Footer
+            </div>
+        </footer>
+    )
+}
+
+function LogoutComponent() {
+    return (
+        <div className="logoutComponent">
+            <h1>You are logged out!!!</h1>
+            <div>Thank you for using our app, come back soon!!!</div>
         </div>
     )
 }
